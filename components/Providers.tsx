f67299a87612665edbd4be0555319b9afbf00afe
Nicholas Brown <nicholasalanbrown@gmail.com>
2026-02-26 13:52:56 -0500

    chore: bump @texturehq/edges to ^1.28.1, prep for darkVariables
    - Bumped edges dep to ^1.28.1
    - Renamed brand var constants from CONTEXT_EXPLORER_ to OPENGRID_
    - Added TODO comment to wire up darkVariables once a new Edges version is cut
      with the merged BrandProvider changes (mono #8322)
    - Kept .theme-dark CSS overrides as interim until that publish happens

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -3,7 +3,7 @@
 import { BrandProvider, ColorModeProvider, NoticeProvider } from "@texturehq/edges";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
 
-const CONTEXT_EXPLORER_BRAND_VARIABLES = {
+const OPENGRID_BRAND_VARIABLES = {
   "--color-brand-primary": "#2563eb",
   "--color-brand-dark": "#1d4ed8",
 };
@@ -12,7 +12,8 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ErrorBoundary level="page" title="Application Error">
       <ColorModeProvider>
-        <BrandProvider variables={CONTEXT_EXPLORER_BRAND_VARIABLES}>
+        {/* TODO: add darkVariables once Edges is published with BrandProvider darkVariables support (mono #8322) */}
+        <BrandProvider variables={OPENGRID_BRAND_VARIABLES}>
           <NoticeProvider>{children}</NoticeProvider>
         </BrandProvider>
       </ColorModeProvider>
